9dc7a34e3aabe84735a2d5368bce24999e46287b
Aura Román <kyradiscord@gmail.com>
2023-02-20 00:28:37 +0100

    feat: added remaining pages

--- components/sections/app-footer.vue
+++ components/sections/app-footer.vue
@@ -3,7 +3,7 @@
 		<div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
 			<p class="text-sm opacity-70 sm:py-2 sm:mt-0 mt-4">
 				© 2023 Skyra Project —
-				<NuxtLink href="https://github.com/skyra-project" class="hover:underline underline-offset-2">GitHub</NuxtLink>
+				<NuxtLink href="https://github.com/skyra-project" class="hover:underline">GitHub</NuxtLink>
 			</p>
 		</div>
 	</footer>
