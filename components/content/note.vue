c4468dbd3ad6efaa748ca331e813e5b00a02a7ef
Aura Román <kyradiscord@gmail.com>
2023-02-19 23:40:23 +0100

    feat: added skyra v7 post

---
+++ components/content/note.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="italic not-prose opacity-70">
+		<slot></slot>
+	</div>
+</template>
--- components/external-link.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<nuxt-link :to="href" target="_blank" class="hover:underline underline-offset-2">
-		{{ text ?? href }}
-	</nuxt-link>
-</template>
-
-<script setup lang="ts">
-defineProps<{ href: string; text?: string }>();
-</script>
