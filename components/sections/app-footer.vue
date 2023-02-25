78dfa23f530a4f4564db81a60ed7d5f69151bb86
Jeroen Claassens <support@favware.tech>
2023-02-25 02:56:57 +0100

    fix: fixed layout for nav, body, and footer

--- components/sections/app-footer.vue
+++ components/sections/app-footer.vue
@@ -1,10 +1,10 @@
 <template>
-	<footer>
-		<div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-			<p class="text-sm opacity-70 sm:py-2 sm:mt-0 mt-4">
+	<footer class="absolute top-[90%] w-full">
+		<div class="flex justify-center">
+			<div class="text-sm opacity-70">
 				© 2023 Skyra Project —
 				<nuxt-link href="https://github.com/skyra-project" class="hover:underline">GitHub</nuxt-link>
-			</p>
+			</div>
 		</div>
 	</footer>
 </template>
--- components/sections/app-header.vue
+++ components/sections/app-header.vue
@@ -1,6 +1,6 @@
 <template>
-	<header class="bg-zinc-100 dark:bg-zinc-900">
-		<div class="container px-5 py-4 mx-auto flex items-center sm:flex-row flex-col">
+	<header class="flex items-center min-h-[4rem] p-2 w-full bg-zinc-100 dark:bg-zinc-900">
+		<div class="container px-5 flex items-center">
 			<p class="text-lg sm:py-2">
 				<nuxt-link href="/" class="font-extrabold text-2xl hover:text-zinc-700 hover:dark:text-zinc-400">Skyra Blog Posts</nuxt-link>
 			</p>
