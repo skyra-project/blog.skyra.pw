7a30f4c49abd0b5b7c37c874acff91fa0f8a625d
Aura Román <kyradiscord@gmail.com>
2023-02-25 12:29:29 +0100

    fix: update .prettierignore, remove yarn generate

--- components/sections/app-footer.vue
+++ components/sections/app-footer.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer>
-		<div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-			<div class="text-sm opacity-70 sm:py-2 sm:mt-0 mt-4">
+		<div class="container mx-auto flex flex-col items-center px-5 py-8 sm:flex-row">
+			<div class="mt-4 text-sm opacity-70 sm:mt-0 sm:py-2">
 				© 2023 Skyra Project —
 				<nuxt-link href="https://github.com/skyra-project" class="hover:underline">GitHub</nuxt-link>
 			</div>
