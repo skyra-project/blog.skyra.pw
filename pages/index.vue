47b1414145f3727f0402d19b1b0367f8982b8926
Aura Román <kyradiscord@gmail.com>
2023-02-25 15:24:34 +0100

    style: display headers correctly on narrower devices

--- pages/index.vue
+++ pages/index.vue
@@ -3,24 +3,28 @@
 	<div class="mx-4 grid gap-3 md:mx-0 lg:grid-cols-2 lg:gap-10">
 		<ContentList v-slot="{ list }" :query="query">
 			<div v-for="article of list" :key="article._path" class="rounded-lg bg-zinc-100 p-4 shadow-md drop-shadow-md dark:bg-zinc-700">
-				<nuxt-link :to="article._path">
-					<img
-						:src="`/images/${article.image}.png`"
-						class="inline h-24 w-24 select-none rounded-lg shadow-lg hover:saturate-150"
-						:alt="article.image"
-					/>
-				</nuxt-link>
-
-				<div class="inline-block pl-4">
-					<nuxt-link :to="article._path" class="mb-3 text-xl font-semibold hover:underline">
-						{{ article.title }}
+				<div class="flex">
+					<nuxt-link :to="article._path" >
+						<img
+							:src="`/images/${article.image}.png`"
+							class="inline select-none rounded-lg shadow-lg hover:saturate-150"
+							width="96"
+							height="96"
+							:alt="article.image"
+						/>
 					</nuxt-link>
-					<p class="mb-6 opacity-80">
-						<small>
-							Published on <u :title="longDate.format(new Date(article.date))">{{ date.format(new Date(article.date)) }}</u> by
-							<span class="opacity-90">{{ article.author }}</span></small
-						>
-					</p>
+
+					<div class="inline-block pl-4">
+						<nuxt-link :to="article._path" class="mb-3 text-xl font-semibold hover:underline">
+							{{ article.title }}
+						</nuxt-link>
+						<p class="mb-6 opacity-80">
+							<small>
+								Published on <u :title="longDate.format(new Date(article.date))">{{ date.format(new Date(article.date)) }}</u> by
+								<span class="opacity-90">{{ article.author }}</span></small
+							>
+						</p>
+					</div>
 				</div>
 
 				<p class="pt-4 opacity-90">
